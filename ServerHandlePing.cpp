1a9419a2bcf75d780ee4674f0b30edb968adf639
Aspegic <mlabrirh@student.1337.ma>
2026-07-18 10:08:45 +0100

    Revert "Refactor IRC command handling into per-command Server translation units"

--- ServerHandlePing.cpp
+++
@@ -1,9 +0,0 @@
-#include "Server.hpp"
-
-void Server::handlePing(int clientFd, const ParsedCommand &command) {
-    if (command.params.empty()) {
-        sendNumeric(clientFd, 409, ":No origin specified");
-        return;
-    }
-    sendRaw(clientFd, ":ircserv PONG ircserv :" + command.params[0] + "\r\n");
-}
